209979fe03ad4acd9db384c788dbf67ec7128909
Rails Practice <rdgill95@gmail.com>
2020-01-02 16:58:26 -0800

    Avatar for users on their page

---
+++ app/helpers/avatar_helper.rb
@@ -0,0 +1,11 @@
+module AvatarHelper
+
+    def avatar(user)
+        email_digest = Digest::MD5.hexdigest user.email
+        gravatar_url = "//www.gravatar.com/avatar/#{email_digest}"
+        
+        image_tag gravatar_url
+        
+    end
+
+end
